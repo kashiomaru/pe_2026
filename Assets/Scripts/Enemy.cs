8c25a7bbdb75c997bf3e1b6058b1e08d5fce7ea6
kashiomaru <shinka0831s@gmail.com>
2026-01-10 16:43:17 +0900

    敵処理対応

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Enemy : MonoBehaviour
 {
+    [Header("Combat")]
     public int maxHp = 3;
+    
+    [Header("Physics")]
+    public float gravity = -9.81f;
+    
     private int _currentHp;
+    private CharacterController _characterController;
+    private Vector3 _velocity;
 
     void Start()
     {
         _currentHp = maxHp;
+        _characterController = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        // 重力を常に適用
+        ApplyGravity();
     }
 
     // ダメージを受ける処理
@@ -29,5 +44,17 @@ public class Enemy : MonoBehaviour
         Debug.Log("Enemy Defeated!");
         // 死亡エフェクトや音を入れるならここ
         Destroy(gameObject);
+    }
+    
+    void ApplyGravity()
+    {
+        // 接地しているなら重力リセット（少し押し付ける）
+        if (_characterController.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        _velocity.y += gravity * Time.deltaTime;
+        _characterController.Move(_velocity * Time.deltaTime);
     }
 }
